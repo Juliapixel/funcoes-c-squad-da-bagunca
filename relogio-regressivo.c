666c7d592596c42eebf2278a1372c399d255a318
Allan <cardoso.souza2109@gmail.com>
2025-04-29 09:21:19 -0300

    Adicionei o protótipo da função

--- relogio-regressivo.c
+++ relogio-regressivo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+void contagemRegressiva(int inicio);
+int main()
+{
+    int numero;
+    printf("\nDigite algum número para dar início a contagem regressiva:");
+    scanf("%d", &numero);
+    contagemRegressiva(numero);
+    return 0;
+}
 void contagemRegressiva(int inicio)
 {
     for (int i = inicio; i >= 0; i--)
@@ -10,15 +19,4 @@ void contagemRegressiva(int inicio)
     }
     printf("Tempo esgotado!\n");
     printf("Fim da contagem.");
-}
-
-int main()
-{
-    int numero;
-    printf("\nDigite algum número para dar início a contagem regressiva:");
-    scanf("%d", &numero);
-
-    contagemRegressiva(numero);
-
-    return 0;
 }
